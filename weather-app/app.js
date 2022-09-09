d819db0cc23502b3e8500f05dc846e583e26de87
Tomas Lancaric <lancaric@hopintaxi.com>
2022-09-09 15:20:23 +0200

    38hotovo

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,13 @@
 const request = require("request")
 const geocode = require ("./utils/geocode")
-// const url = "http://api.weatherstack.com/current?access_key=5f92cd780c8d5b89e2034fb50d1230ec&query=48.50617,17.430404"
-
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log("Nemáš internet debile!")
-//     } else if (response.body.error) {
-//         console.log("To si si z jakej rici vytáhol? Šak to nevím nájst!")
-//     }
-//     else {
-//         console.log(response.body.current.weather_descriptions[0] + ". Ščuleky je " + response.body.current.temperature + " stupnóf. " + "Cícíš to jak keby bolo " + response.body.current.feelslike + " stupnóf.")
-//     }
-// })
-
+const forecast = require ("./utils/forecast.js")
 
 geocode("Lošonec", (error, data) => {
     console.log("Error", error)
     console.log("Data", data)
 })
+
+forecast(48.483333, 17.4, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require ("request")
+
+const forecast = (latitude, longitude, callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=5f92cd780c8d5b89e2034fb50d1230ec&query=" + latitude + "," + longitude + "&units=m"
+
+request({ url: url, json: true }, (error, response) => {
+    if (error) {
+        callback("Nemáš internet debil, jak sceš dostat počasí!", undefined)
+    } else if (response.body.error) {
+        callback("To si si z jakej rici vytáhol? Šak to nevím nájst!", undefined)
+    } else {
+        callback(undefined, response.body.current.weather_descriptions[0] + ". Ščuleky je " + response.body.current.temperature + " stupnóf. " + "Cícíš to jak keby bolo " + response.body.current.feelslike + " stupnóf.")
+    }
+})
+}
+module.exports = forecast
