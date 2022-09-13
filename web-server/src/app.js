2f2d267b19307061e5b6c1656a4be7d2c00d822f
Tomas Lancaric <lancaric@hopintaxi.com>
2022-09-13 10:52:14 +0200

    52

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const express = require("express")
 const hbs = require("hbs")
+const { response } = require("express")
 
 const app = express()
 
@@ -19,8 +20,8 @@ app.use(express.static(publicDirectoryPath))
 
 app.get("", (req, res) => {
     res.render("index", {
-        title: "Pošťastie",
-        name: "Tomas"
+        title: "Pošťasie",
+        name: "doTLA"
     })
 })
 
@@ -31,10 +32,17 @@ app.get("/help", (req, res) => {
         })
 })
 
+app.get("/help/me", (req, res) => {
+    res.render("Pomoc", {
+        title: "Máte smúlu",
+        name: "ser na to a zomri"
+        })
+})
+
 app.get("/about", (req, res) => {
     res.render("about", {
-        title: "O mne",
-        name: "Tomas"
+        title: "Iný obrázok nemám",
+        name: "doTLA"
     })
 })
 
@@ -42,6 +50,23 @@ app.get("/weather", (req, res) => {
     res.send({
         forecast: "Svící",
         location: "Šadze"
+    })
+})
+
+app.get("/help/*", (req, res) => {
+    res.render("helpme", {
+        title: "Pomoc sa nenašla",
+        name: "ser na to a zomri",
+        chybnaSprava: "Všetko biele (Ján Smrek)  Až umriem raz, nech je to v máji, keď kvitne biely orgován. Bo dnes som videl veniec- krásny!- niesli ho, dvere dokorán, a mňa ten veniec očaroval: vznešený biely velikán, v ňom orgován len samý biely a pri ňom biely tulipán!"
+    })
+})
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: "doTLA",
+        chybnaSprava: "Takto debilnú stránku môžeš zadať iba ty -_-"
+
     })
 })
 
